19f8688f613c5bbee24d189d2f718beb3158a67a
tmelliott <tom.elliott@auckland.ac.nz>
2022-02-10 20:39:45 +0000

    tmelliott published a site update

--- emails/workshop2022/sendemail.R
+++ emails/workshop2022/sendemail.R
@@ -2,12 +2,19 @@ e <- paste(readLines('email.txt'), collapse = "\n")
 txt <- paste(readLines('index.txt'), collapse = "\n")
 html <- paste(readLines('index.html'), collapse = "\n")
 
+library(dotenv)
+invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
+email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = 4L) |>
+    dplyr::filter(!Sent & Email != "")
+
+# email_list <- data.frame(Email = c("tom.elliott@auckland.ac.nz", "tom.elliott@vuw.ac.nz", "tomelliottnz@gmail.com"))
+emails <- paste(email_list$Email, collapse = ", ")
+
+e <- gsub("%EMAILS%", emails, e, fixed = TRUE)
 e <- gsub("%TEXT%", txt, e, fixed = TRUE)
 e <- gsub("%HTML%", html, e, fixed = TRUE)
 cat(e, "\n", file = "tosend.txt")
 
-message("Remember to use 'https://htmlcrush.com/light/' first ...")
-
 cat("Sending mail ...\n")
-system('cat tosend.txt | sendmail -t')
+# system('cat tosend.txt | sendmail -t')
 cat("\nDone!\n")
