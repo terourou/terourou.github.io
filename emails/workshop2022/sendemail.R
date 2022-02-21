454a6cf5841a9f915018fcb49ad6c58bd8904894
tmelliott <tom.elliott@auckland.ac.nz>
2022-02-21 21:23:35 +0000

    tmelliott published a site update

--- emails/workshop2022/sendemail.R
+++ emails/workshop2022/sendemail.R
@@ -2,16 +2,13 @@ e <- paste(readLines('email.txt'), collapse = "\n")
 txt <- paste(readLines('index.txt'), collapse = "\n")
 html <- paste(readLines('index.html'), collapse = "\n")
 
-# library(dotenv)
-# invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
-# email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = 4L) |>
-#     dplyr::filter(!Sent & Email != "")
+library(dotenv)
+invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
+email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = 4L) |>
+    dplyr::filter(!Sent & Email != "")
 
-# email_list <- data.frame(Email = c("tom.elliott@auckland.ac.nz"))
-# emails <- paste(email_list$Email, collapse = ", ")
-emails <- ""
-
-message("TEMPORARY USE ONLY - EMAIL IS UGLY")
+# email_list <- data.frame(Email = c("tom.elliott@auckland.ac.nz", "tom.elliott@vuw.ac.nz", "tomelliottnz@gmail.com"))
+emails <- paste(email_list$Email, collapse = ", ")
 
 e <- gsub("%EMAILS%", emails, e, fixed = TRUE)
 e <- gsub("%TEXT%", txt, e, fixed = TRUE)
