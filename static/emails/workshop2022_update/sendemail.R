93c34cc94e0ecb18357b17c2671841e2a9b6282f
Tom Elliott <tomelliottnz@gmail.com>
2022-03-28 21:38:48 +1300

    final email

--- static/emails/workshop2022_update/sendemail.R
+++ static/emails/workshop2022_update/sendemail.R
@@ -2,16 +2,17 @@ e <- paste(readLines('email.txt'), collapse = "\n")
 txt <- paste(readLines('index.txt'), collapse = "\n")
 html <- paste(readLines('index.html'), collapse = "\n")
 
-# library(dotenv)
-# invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
+library(dotenv)
+invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
 # email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = 4L) |>
-#     dplyr::filter(!Sent & Email != "")
+#     dplyr::filter()
 
+# email_list <- data.frame(Email = c("a.sporle@auckland.ac.nz", "b.milne@auckland.ac.nz", "colinrsimpson@gmail.com", "tom.elliott@auckland.ac.nz", "Lucy.Nelson@swa.govt.nz"))
 email_list <- data.frame(Email = c("tom.elliott@auckland.ac.nz"))
 emails <- paste(email_list$Email, collapse = ", ")
 # emails <- ""
 
-message("MAKE SURE YOU DO THE TEXT VERSION ONCE FINALISED!!")
+message("REMOVE DRAFT TEXT BEFORE SENDING!!!! (in email.txt)")
 
 e <- gsub("%EMAILS%", emails, e, fixed = TRUE)
 e <- gsub("%TEXT%", txt, e, fixed = TRUE)
@@ -19,5 +20,5 @@ e <- gsub("%HTML%", html, e, fixed = TRUE)
 cat(e, "\n", file = "tosend.txt")
 
 cat("Sending mail ...\n")
-# system('cat tosend.txt | sendmail -t')
+system('cat tosend.txt | sendmail -t')
 cat("\nDone!\n")
