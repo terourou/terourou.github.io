e8e67baea42c1dff0a16b4d94642fece918c58ab
tmelliott <tom.elliott@auckland.ac.nz>
2022-11-18 03:08:26 +0000

    tmelliott published a site update

--- emails/symposium_2023/send_email.R
+++ emails/symposium_2023/send_email.R
@@ -1,13 +1,13 @@
-e <- paste(readLines('email.txt'), collapse = "\n")
+# e <- paste(readLines('email.txt'), collapse = "\n")
+
+library(dotenv)
+
 txt <- paste(readLines('save_the_date.txt'), collapse = "\n")
 html <- paste(readLines('save_the_date.min.html'), collapse = "\n")
-
 subject <- "Save The Data - Symposium 'Our Data Sources as a Strategic National Asset' - March 9 2023"
 
 CONFIRM <- Sys.getenv("CONFIRM")
 if (CONFIRM == "TRUE") {
-
-    library(dotenv)
     invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
     email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = "Attendees") |>
         dplyr::filter(!is.na(Email) & Email != "" & Type %in% c("Organiser", "Attendee"))
@@ -29,17 +29,57 @@ if (CONFIRM == "TRUE") {
 
     emails <- paste(
         c(
-            "tom.elliott@auckland.ac.nz"
+            "tom.elliott@auckland.ac.nz",
+            # "colin.simpson@vuw.ac.nz",
+            # "a.sporle@auckland.ac.nz",
+            # "b.milne@auckland.ac.nz",
+            # "robin.blythe@qut.edu.au",
+            # "Louise.Pirini@swa.govt.nz",
+            NULL
         )
     )
+    message("Sending to: ", paste(emails, collapse = ", "))
 }
 
-e <- gsub("%EMAILS%", emails, e, fixed = TRUE)
-e <- gsub("%SUBJECT%", subject, e, fixed = TRUE)
-e <- gsub("%TEXT%", txt, e, fixed = TRUE)
-e <- gsub("%HTML%", html, e, fixed = TRUE)
-cat(e, "\n", file = "tosend.txt")
+# Send the email
+json_template <- list(
+    personalizations = list(list(
+        to = list(list(
+            email = jsonlite::unbox("terourounz@gmail.com"),
+            name = jsonlite::unbox("Te Rourou Tātaritanga")
+        )),
+        bcc = lapply(emails, \(e) list(email = jsonlite::unbox(e)))
+    )),
+    from = list(
+        email = jsonlite::unbox("terourounz@gmail.com"),
+        name = jsonlite::unbox("Te Rourou Tātaritanga")
+    ),
+    subject = jsonlite::unbox(subject),
+    content = list(
+        list(
+            type = jsonlite::unbox("text/plain"),
+            value = jsonlite::unbox(txt)
+        ),
+        list(
+            type = jsonlite::unbox("text/html"),
+            value = jsonlite::unbox(html)
+        )
+    )
+)
 
-message("Sending mail ...")
-system('cat tosend.txt | sendmail -t')
-message("Done!")
+cat(jsonlite::toJSON(json_template, pretty = TRUE), file = "email.json")
+
+cmd <- sprintf(
+        paste(
+            "curl -s --request POST",
+            "--url https://api.sendgrid.com/v3/mail/send",
+            "--header \"Authorization: Bearer %s\"",
+            "--header 'Content-Type: application/json'",
+            "--data \"$(cat email.json)\""
+        ),
+        Sys.getenv("SENDGRID_API_KEY")
+    )
+
+message("Sending email...")
+system(cmd)
+message("Email sent.")
