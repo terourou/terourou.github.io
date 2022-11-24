8dce2139b1a42802660beba9c3a8c0562454dd9e
tmelliott <tom.elliott@auckland.ac.nz>
2022-11-24 03:25:45 +0000

    tmelliott published a site update

--- emails/symposium_2023/send_email.R
+++ emails/symposium_2023/send_email.R
@@ -9,76 +9,100 @@ subject <- "Save The Date - Symposium 'Our Data Sources as a Strategic National 
 CONFIRM <- Sys.getenv("CONFIRM")
 if (CONFIRM == "TRUE") {
     invite_list_csv <- Sys.getenv("GOOGLE_SHEET")
-    email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = "Attendees") |>
+    email_list <- googlesheets4::read_sheet(invite_list_csv, sheet = "Blocked emails") |>
         dplyr::filter(!is.na(Email) & Email != "")
 
     # find column starting with 'Invite?' and select all rows where value is TRUE
-    email_list <- email_list[email_list[[6]], ]
+    # email_list <- email_list[email_list[[6]], ]
 
     message(sprintf("You are about to send this email to %i people. Are you sure? (y/N)", nrow(email_list)))
 
     if (tolower(readline()) == "y") {
-        emails <- email_list$Email
+        # emails <- email_list$Email
+        emails <- "tom.elliott@auckland.ac.nz"
     } else {
         email <- ""
         stop()
     }
 } else {
     message("This is a test run. Set CONFIRM=TRUE to send emails to the list.")
-    subject <- paste("[FINAL DRAFT - sending just after lunch]", subject)
+    subject <- paste("[TEST]", subject)
 
-    emails <- paste(
-        c(
-            "tom.elliott@auckland.ac.nz",
-            # "colin.simpson@vuw.ac.nz",
-            # "a.sporle@auckland.ac.nz",
-            # "b.milne@auckland.ac.nz",
-            # "robin.blythe@qut.edu.au",
-            # "Louise.Pirini@swa.govt.nz",
-            NULL
-        )
+    emails <- c(
+        # "tomelliottnz@gmail.com",
+        "tom.elliott@vuw.ac.nz",
+        # "colin.simpson@vuw.ac.nz",
+        # "a.sporle@auckland.ac.nz",
+        # "b.milne@auckland.ac.nz",
+        # "robin.blythe@qut.edu.au",
+        # "Louise.Pirini@swa.govt.nz",
+        NULL
     )
+
     message("Sending to: ", paste(emails, collapse = ", "))
 }
 
-# Send the email
-json_template <- list(
-    personalizations = list(list(
-        to = list(list(
-            email = jsonlite::unbox("terourounz@gmail.com"),
-            name = jsonlite::unbox("Te Rourou Tātaritanga")
-        )),
-        bcc = lapply(emails, \(e) list(email = jsonlite::unbox(e)))
-    )),
-    from = list(
-        email = jsonlite::unbox("terourounz@gmail.com"),
-        name = jsonlite::unbox("Te Rourou Tātaritanga")
-    ),
-    subject = jsonlite::unbox(subject),
-    content = list(
-        list(
-            type = jsonlite::unbox("text/plain"),
-            value = jsonlite::unbox(txt)
-        ),
-        list(
-            type = jsonlite::unbox("text/html"),
-            value = jsonlite::unbox(html)
-        )
-    )
-)
+## This is needed for SENDGRID
+# # Send the email
+# json_template <- list(
+#     personalizations = list(list(
+#         to = list(list(
+#             email = jsonlite::unbox("terourounz@gmail.com"),
+#             name = jsonlite::unbox("Te Rourou Tātaritanga")
+#         )),
+#         bcc = lapply(emails, \(e) list(email = jsonlite::unbox(e)))
+#     )),
+#     from = list(
+#         # email = jsonlite::unbox("noreply@terourou.org"),
+#         email = jsonlite::unbox("terourounz@gmail.com"),
+#         name = jsonlite::unbox("Te Rourou Tātaritanga")
+#     ),
+#     subject = jsonlite::unbox(subject),
+#     content = list(
+#         list(
+#             type = jsonlite::unbox("text/plain"),
+#             value = jsonlite::unbox(txt)
+#         ),
+#         list(
+#             type = jsonlite::unbox("text/html"),
+#             value = jsonlite::unbox(html)
+#         )
+#     )
+# )
 
-cat(jsonlite::toJSON(json_template, pretty = TRUE), file = "email.json")
+# cat(jsonlite::toJSON(json_template, pretty = TRUE), file = "email.json")
 
+# cmd <- sprintf(
+#         paste(
+#             "curl -s --request POST",
+#             "--url https://api.sendgrid.com/v3/mail/send",
+#             "--header \"Authorization: Bearer %s\"",
+#             "--header 'Content-Type: application/json'",
+#             "--data \"$(cat email.json)\""
+#         ),
+#         Sys.getenv("SENDGRID_API_KEY")
+#     )
+
+
+## THIS IS NEEDED FOR MAILGUN
 cmd <- sprintf(
-        paste(
-            "curl -s --request POST",
-            "--url https://api.sendgrid.com/v3/mail/send",
-            "--header \"Authorization: Bearer %s\"",
-            "--header 'Content-Type: application/json'",
-            "--data \"$(cat email.json)\""
-        ),
-        Sys.getenv("SENDGRID_API_KEY")
-    )
+    paste(
+        "curl -s --user 'api:%s'",
+        "https://api.mailgun.net/v3/%s/messages",
+        "-F from='Te Rourou Tātaritanga <contact@terourou.org>'",
+        # "-F h:Reply-To='Te Rourou Tātaritanga <terourounz@gmail.com>'",
+        "-F to='Te Rourou Tātaritanga <contact@terourou.org>'",
+        "-F bcc=\"%s\"",
+        "-F subject=\"%s\"",
+        "-F text=\"%s\"",
+        "--form-string html=\"$(cat save_the_date.min.html)\""
+    ),
+    Sys.getenv("MAILGUN_API_KEY"),
+    "terourou.org",
+    paste(emails, collapse = ","),
+    subject,
+    txt
+)
 
 message("Sending email...")
 system(cmd)
